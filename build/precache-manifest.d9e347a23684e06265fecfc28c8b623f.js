eb4b10d138e76288f729e6d5169f93f2bf395781
eric <eric.autoprog@gmail.com>
2023-02-14 14:12:11 +0100

    init 3

---
+++ build/precache-manifest.d9e347a23684e06265fecfc28c8b623f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a64c57448613304ab717db0cfa3e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad9f3a61d11df867239a",
+    "url": "/static/css/main.83810d5c.chunk.css"
+  },
+  {
+    "revision": "9f3c4d98388010269609",
+    "url": "/static/js/2.aca2be11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aca2be11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9f3a61d11df867239a",
+    "url": "/static/js/main.e778cca2.chunk.js"
+  },
+  {
+    "revision": "a1c0900928d8432985db",
+    "url": "/static/js/runtime-main.6aebae31.js"
+  },
+  {
+    "revision": "cb80d85bc965a10b09e43f3076173b6c",
+    "url": "/static/media/ericS2julyna.cb80d85b.jpg"
+  },
+  {
+    "revision": "d348e4401884dd1bb421bbe513fa54a7",
+    "url": "/static/media/pandinha.d348e440.gif"
+  },
+  {
+    "revision": "f1be026ff08aab1125b691c069d382f0",
+    "url": "/static/media/pandinha_coracao.f1be026f.gif"
+  },
+  {
+    "revision": "46feb314ef66fd9844e6c5343e228597",
+    "url": "/static/media/pandinha_fofa_cara_de_brava.46feb314.gif"
+  },
+  {
+    "revision": "8b0c4eecc314fa3131eae4e8ec70d06a",
+    "url": "/static/media/pandinha_martelo.8b0c4eec.png"
+  },
+  {
+    "revision": "865f1c977c2083c42cda7c223f72ea76",
+    "url": "/static/media/ursinha_chorando.865f1c97.gif"
+  },
+  {
+    "revision": "c4ff310ed4399ebe2c85ffc52ad5eeab",
+    "url": "/static/media/ursinha_mordidas.c4ff310e.gif"
+  },
+  {
+    "revision": "641a584c05345c733e8e5d94f906e53c",
+    "url": "/static/media/ursinho_S2_pandinha.641a584c.gif"
+  },
+  {
+    "revision": "7746f73129cc8fc7f46ada5e23f4d1b5",
+    "url": "/static/media/ursinho_catucando_pandinha.7746f731.gif"
+  },
+  {
+    "revision": "ee3bbcefdd762dc554b5db84df32bded",
+    "url": "/static/media/ursinho_chorando.ee3bbcef.gif"
+  },
+  {
+    "revision": "bde071c432bb0a09b34af0e0d5ffcd98",
+    "url": "/static/media/ursinho_estudando.bde071c4.gif"
+  },
+  {
+    "revision": "81e2399aba3f1ff6cd0c1df8eb967412",
+    "url": "/static/media/ursinho_pandinha_ok.81e2399a.gif"
+  },
+  {
+    "revision": "387281264ae4000e8a803a40169c1ac8",
+    "url": "/static/media/ursinho_pegando_no_sono.38728126.gif"
+  },
+  {
+    "revision": "04b613c4523e12dcb35d7f703a752faf",
+    "url": "/static/media/ursinhos_apaixonados.04b613c4.gif"
+  },
+  {
+    "revision": "e831c6c0da241c5146771d0233cfc219",
+    "url": "/static/media/ursinhos_bebida.e831c6c0.gif"
+  },
+  {
+    "revision": "b887f38a5ae81d5a3fe91244955386dc",
+    "url": "/static/media/ursinhos_caminhada.b887f38a.jpg"
+  },
+  {
+    "revision": "73252a13157ecdec88497a19c35256e5",
+    "url": "/static/media/ursinhos_dormindo.73252a13.gif"
+  },
+  {
+    "revision": "4aa4f4ad4538a66d10b1537ea06d994c",
+    "url": "/static/media/ursinhos_fotinha.4aa4f4ad.jpg"
+  }
+]);
